73aae347ce5e4b1624d20abbdf5ff82eaae4cdf6
Julien Palmas <julien@crx.io>
2020-04-21 12:16:51 +0200

    dynamic detection of the fomantic-ui* package used

--- packages/ember-fui-modules/index.js
+++ packages/ember-fui-modules/index.js
@@ -6,14 +6,48 @@ const debug = require("debug")(name);
 module.exports = {
   name,
 
-  importFuiModule(moduleName, importCss, includer) {
-    const modulesDir = "node_modules/fomantic-ui-css/components";
+  importCss: true,
+
+  usedFomanticUIPackage(project) {
+    const allDeps = Object.keys(project.pkg.devDependencies).concat(
+      Object.keys(project.pkg.dependencies || {})
+    );
+
+    if (allDeps.includes("fomantic-ui-less")) return "fomantic-ui-less";
+    if (allDeps.includes("fomantic-ui-css")) return "fomantic-ui-css";
+    if (allDeps.includes("fomantic-ui")) return "fomantic-ui";
+
+    throw "It looks like you application does not depend on any FUI package providing the modules files. Make sure you depend on either fomantic-ui, fomantic-ui-css or fomantic-ui-less if you want to use ember-fui-modules";
+  },
+
+  // The fomantic-ui and fomantic-ui-css packages distribute the module files under the directory `components` while fomantic-ui-less uses `definitions/modules`
+  fomanticUIModulesDir(project) {
+    const usedFomanticUIPackage = this.usedFomanticUIPackage(project);
+
+    if (usedFomanticUIPackage === "fomantic-ui-less")
+      return "node_modules/fomantic-ui-less/definitions/modules";
+
+    if (["fomantic-ui-css", "fomantic-ui"].includes(usedFomanticUIPackage))
+      return `node_modules/${usedFomanticUIPackage}/components`;
+
+    throw "It looks like you application does not depend on any FUI package providing the modules files. Make sure you depend on either fomantic-ui, fomantic-ui-css or fomantic-ui-less if you want to use ember-fui-modules";
+  },
+
+  shouldImportCss(includer) {
+    if (this.usedFomanticUIPackage(includer.project) === "fomantic-ui-less")
+      return false;
+
+    return this.importCss;
+  },
+
+  importFuiModule(moduleName, includer) {
+    const modulesDir = this.fomanticUIModulesDir(includer.project);
     const importedModule = `${modulesDir}/${moduleName}.js`;
 
     debug("importing %o for '%s'", importedModule, includer.name);
     this.import(importedModule);
 
-    if (importCss) {
+    if (this.shouldImportCss(includer)) {
       let importedCss = `${modulesDir}/${moduleName}.css`;
       debug("importing %o for '%s'", importedCss, includer.name);
       this.import(importedCss);
@@ -23,45 +57,18 @@ module.exports = {
   included(includer) {
     this._super.included.apply(this, includer);
 
-    if (!includer.options.fuiModules) return;
+    const buildOptions = includer.options.fuiModules;
+    if (!buildOptions) return;
 
-    this.fuiModulesOptions = includer.options.fuiModules;
-    this.fuiModulesOptions.only = this.fuiModulesOptions.only || [];
+    // by default, we do not import any module. Devs must be specific about what modules their app needs. This is to avoid the includer's bundle to be filled with unused JS
+    let modulesToImport = new Set(buildOptions.only);
 
-    // importCss defaults to true
-    if (this.fuiModulesOptions.importCss === undefined)
-      this.fuiModulesOptions.importCss = true;
+    // Some FUI modules depend on others to work
+    if (modulesToImport.has("modal")) modulesToImport.add("dimmer");
+    if (modulesToImport.has("popup")) modulesToImport.add("transition");
 
-    this.fuiModulesOptions.only.forEach((moduleName) => {
-      this.importFuiModule(
-        moduleName,
-        this.fuiModulesOptions.importCss,
-        includer
-      );
-
-      // modal.js requires dimmer.js
-      if (
-        !this.fuiModulesOptions.only.includes("dimmer") &&
-        moduleName === "modal"
-      ) {
-        this.importFuiModule(
-          "dimmer",
-          this.fuiModulesOptions.importCss,
-          includer
-        );
-      }
-
-      // popup.js requires transition.js
-      if (
-        !this.fuiModulesOptions.only.includes("transition") &&
-        moduleName === "popup"
-      ) {
-        this.importFuiModule(
-          "transition",
-          this.fuiModulesOptions.importCss,
-          includer
-        );
-      }
+    modulesToImport.forEach((moduleName) => {
+      this.importFuiModule(moduleName, includer);
     });
   },
 };
--- test-packages/test-app-with-fomantic-ui-less/ember-cli-build.js
+++ test-packages/test-app-with-fomantic-ui-less/ember-cli-build.js
@@ -1,10 +1,12 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    fuiModules: {
+      only: ["slider"],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
