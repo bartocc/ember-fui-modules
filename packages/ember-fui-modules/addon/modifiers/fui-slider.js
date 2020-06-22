cc07b3d2d150c9d4ab5e7062201d6fd005e22e3c
Julien Palmas <julien@crx.io>
2020-06-22 11:35:55 +0200

    reference $.fn.slider.settings.interpretLabel issue

--- packages/ember-fui-modules/addon/modifiers/fui-slider.js
+++ packages/ember-fui-modules/addon/modifiers/fui-slider.js
@@ -3,6 +3,7 @@ import FuiModuleModifier from "./fui-module";
 export default class FuiSliderModifier extends FuiModuleModifier {
   semanticModuleName = "slider";
 
+  // see https://github.com/fomantic/Fomantic-UI/issues/1526#issuecomment-646082418
   settingExistsOnModule(setting) {
     if (setting === "interpretLabel") return true;
 
