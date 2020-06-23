c5a5f0cd5b94c8a31ace0b20c99a6719c1b9c413
Julien Palmas <julien@crx.io>
2020-06-23 09:56:28 +0200

    ✅ dropdown and slider tests

--- packages/ember-fui-modules/tests/integration/modifiers/fui-slider-test.js
+++ packages/ember-fui-modules/tests/integration/modifiers/fui-slider-test.js
@@ -1,3 +1,5 @@
+/* eslint-disable ember/no-jquery */
+import $ from "jquery";
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
 import { render } from "@ember/test-helpers";
@@ -6,8 +8,14 @@ import hbs from "htmlbars-inline-precompile";
 module("Integration | Modifier | fui-slider", function (hooks) {
   setupRenderingTest(hooks);
 
-  test("using the modifier on a div.ui.slider", async function (assert) {
-    await render(hbs`<div {{fui-slider}} class="ui slider" ></div>`);
-    assert.dom(".ui.slider .inner").exists("initializes the module");
+  test("slider", async function (assert) {
+    await render(hbs`
+      <div class="ui slider" id="slider-1" {{fui-slider}}></div>
+      <div class="ui input">
+        <input type="text" id="slider-input-1" disabled="">
+      </div>
+    `);
+
+    assert.ok($(".ui.slider").data("module-slider"));
   });
 });
