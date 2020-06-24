068cc5bb170de475d877e1bb727fed95979b508d
Julien Palmas <julien@crx.io>
2020-06-24 09:58:43 +0200

    ✅ progress test

--- packages/ember-fui-modules/tests/integration/modifiers/fui-progress-test.js
+++ packages/ember-fui-modules/tests/integration/modifiers/fui-progress-test.js
@@ -1,3 +1,5 @@
+/* eslint-disable ember/no-jquery */
+import $ from "jquery";
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
 import { render } from "@ember/test-helpers";
@@ -6,8 +8,16 @@ import hbs from "htmlbars-inline-precompile";
 module("Integration | Modifier | fui-progress", function (hooks) {
   setupRenderingTest(hooks);
 
-  test("using the modifier on a div.ui.slider", async function (assert) {
-    await render(hbs`<div {{fui-slider}} class="ui slider" ></div>`);
-    assert.dom(".ui.slider .inner").exists("initializes the module");
+  test("progress", async function (assert) {
+    await render(hbs`
+      <div class="ui progress" {{fui-progress}}>
+        <div class="bar">
+          <div class="progress"></div>
+        </div>
+        <div class="label">Uploading Files</div>
+      </div>
+    `);
+
+    assert.ok($(".ui.progress").data("module-progress"));
   });
 });
