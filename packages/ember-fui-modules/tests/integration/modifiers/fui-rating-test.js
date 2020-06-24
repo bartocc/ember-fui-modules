872a4dfb042e0901bd4bbdd5b5caf25ee0bfb79d
Julien Palmas <julien@crx.io>
2020-06-24 10:00:20 +0200

    ✅ rating test

--- packages/ember-fui-modules/tests/integration/modifiers/fui-rating-test.js
+++ packages/ember-fui-modules/tests/integration/modifiers/fui-rating-test.js
@@ -1,3 +1,5 @@
+/* eslint-disable ember/no-jquery */
+import $ from "jquery";
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
 import { render } from "@ember/test-helpers";
@@ -6,8 +8,11 @@ import hbs from "htmlbars-inline-precompile";
 module("Integration | Modifier | fui-rating", function (hooks) {
   setupRenderingTest(hooks);
 
-  test("using the modifier on a div.ui.slider", async function (assert) {
-    await render(hbs`<div {{fui-slider}} class="ui slider" ></div>`);
-    assert.dom(".ui.slider .inner").exists("initializes the module");
+  test("rating", async function (assert) {
+    await render(hbs`
+      <div class="ui rating" data-max-rating="1" {{fui-rating}}></div>
+    `);
+
+    assert.ok($(".ui.rating").data("module-rating"));
   });
 });
